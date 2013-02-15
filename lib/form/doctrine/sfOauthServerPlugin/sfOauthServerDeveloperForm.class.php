6f40bd102e0d56defb1aac2e2634c308dd121f4a
vladimir <workvladster@gmail.com>
2013-02-16 01:24:32 +0300

    add oauth

---
+++ lib/form/doctrine/sfOauthServerPlugin/sfOauthServerDeveloperForm.class.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * sfOauthServerDeveloper form.
+ *
+ * @package    api
+ * @subpackage form
+ * @author     Vladimir
+ * @version    SVN: $Id: sfDoctrinePluginFormTemplate.php 23810 2009-11-12 11:07:44Z Kris.Wallsmith $
+ */
+class sfOauthServerDeveloperForm extends PluginsfOauthServerDeveloperForm
+{
+  public function configure()
+  {
+  }
+}
